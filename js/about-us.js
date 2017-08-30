f34a9ac02816599c7e62bcd87041aa1fcef558e8
Vladanyes <mishoppro@gmail.com>
2017-08-30 11:12:31 +0600

    bb

--- js/about-us.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by vladanyes on 18.07.17.
- */
-// $('h4').next().hide();
-// $('h4').click(function(){
-//     $(this).next().slideToggle();
-//     $('h4').not(this).next().stop(true,true).slideUp();
-// });
